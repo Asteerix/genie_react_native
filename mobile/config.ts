652ec0733858b4adbd6672838ff88e90ab9c3a8d
asteerix <asteerix@amaurys-mbp.home>
2025-04-08 16:07:46 +0200

    [ADD] Adding all missing files

--- mobile/config.ts
+++ mobile/config.ts
@@ -4,13 +4,13 @@ import { Platform } from 'react-native';
 // URLs de l'API pour les différents environnements
 const API_URLS = {
   development: 'http://217.182.129.10:8081',
-  local: Platform.OS === 'android' ? 'http://10.0.2.2:8080' : 'http://217.182.129.10:8081',
-  staging: 'https://staging-api.genie-app.com',
-  production: 'https://api.genie-app.com'
+  local: 'http://217.182.129.10:8081', // Utilise toujours cette URL pour l'environnement local
+  staging: 'http://217.182.129.10:8081',
+  production: 'http://217.182.129.10:8081'
 };
 
 // Sélectionner l'environnement : 'development', 'local', 'staging', ou 'production'
-const ENVIRONMENT = 'local';
+const ENVIRONMENT = 'production';
 
 // Exporter l'URL de l'API en fonction de l'environnement
 export const API_BASE_URL = API_URLS[ENVIRONMENT];
